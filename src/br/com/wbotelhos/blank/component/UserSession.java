5a28bff13b2022564dca3680de2099fed0bce7f6
Washington Botelho <wbotelhos@gmail.com>
2011-03-27 19:54:02 -0300

    Adicionado método de logout.

--- src/br/com/wbotelhos/blank/component/UserSession.java
+++ src/br/com/wbotelhos/blank/component/UserSession.java
@@ -1,19 +1,25 @@
 package br.com.wbotelhos.blank.component;
 
+import java.io.Serializable;
+
 import br.com.caelum.vraptor.ioc.Component;
 import br.com.caelum.vraptor.ioc.SessionScoped;
 import br.com.wbotelhos.blank.model.Usuario;
 
 @Component
 @SessionScoped
-public class UserSession implements java.io.Serializable {
+public class UserSession implements Serializable {
 
-	private static final long serialVersionUID = -7840810249105080395L;
+	private static final long serialVersionUID = -2122338680139826090L;
 
 	private Usuario user;
 
 	public boolean isLogged() {
 		return user != null;
+	}
+
+	public void logout() {
+		user = null;
 	}
 
 	public Usuario getUser() {
