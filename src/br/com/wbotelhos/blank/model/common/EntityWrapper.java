d27b2dd9b4c471f8ac75b54473b95c11b7d7f036
Washington Botelho <wbotelhos@gmail.com>
2011-03-28 15:14:36 -0300

    Adicionado Wrapper de serialização para listagem.

---
+++ src/br/com/wbotelhos/blank/model/common/EntityWrapper.java
@@ -0,0 +1,27 @@
+package br.com.wbotelhos.blank.model.common;
+
+import java.util.Collection;
+
+
+public class EntityWrapper<T extends AbstractEntity> {
+
+	private Collection<T> entityList;
+	private Integer total;
+
+	public Collection<T> getEntityList() {
+		return entityList;
+	}
+
+	public void setEntityList(Collection<T> entityList) {
+		this.entityList = entityList;
+	}
+
+	public Integer getTotal() {
+		return total;
+	}
+
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+
+}
