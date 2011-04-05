106f19f8ac85a4ea023123739fb69324170fa3fa
Washington Botelho <wbotelhos@gmail.com>
2011-04-05 03:04:51 -0300

    Agora o usuário terá ações de imagem, tendo assim um novo repositório para implementar.

--- src/br/com/wbotelhos/starting/repository/UsuarioRepository.java
+++ src/br/com/wbotelhos/starting/repository/UsuarioRepository.java
@@ -1,8 +1,9 @@
 package br.com.wbotelhos.starting.repository;
 
 import br.com.wbotelhos.starting.model.Usuario;
+import br.com.wbotelhos.starting.repository.common.GenericImageRepository;
 import br.com.wbotelhos.starting.repository.common.GenericRepository;
 
-public interface UsuarioRepository extends GenericRepository<Usuario> {
+public interface UsuarioRepository extends GenericRepository<Usuario>, GenericImageRepository<Usuario> {
 
 }
