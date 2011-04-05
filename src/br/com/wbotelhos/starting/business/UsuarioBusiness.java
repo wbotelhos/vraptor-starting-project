7a0350035dac67d3dbdd7dbee814ba9f632c7077
Washington Botelho <wbotelhos@gmail.com>
2011-04-05 03:08:25 -0300

    Herança das regras de negócios das imagens.

--- src/br/com/wbotelhos/starting/business/UsuarioBusiness.java
+++ src/br/com/wbotelhos/starting/business/UsuarioBusiness.java
@@ -3,12 +3,12 @@ package br.com.wbotelhos.starting.business;
 import javax.persistence.EntityManager;
 
 import br.com.caelum.vraptor.ioc.Component;
-import br.com.wbotelhos.starting.business.common.GenericBusiness;
+import br.com.wbotelhos.starting.business.common.GenericImageBusiness;
 import br.com.wbotelhos.starting.model.Usuario;
 import br.com.wbotelhos.starting.repository.UsuarioRepository;
 
 @Component
-public class UsuarioBusiness extends GenericBusiness<Usuario> implements UsuarioRepository {
+public class UsuarioBusiness extends GenericImageBusiness<Usuario> implements UsuarioRepository {
 
 	public UsuarioBusiness(EntityManager manager) {
 		super(manager);
