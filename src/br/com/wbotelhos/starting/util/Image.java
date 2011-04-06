ae351d6bb3f136f29c1156b7127a25cb74afdd71
Washington Botelho <wbotelhos@gmail.com>
2011-04-06 17:58:40 -0300

    Refatorado utilidades de imagem.

--- src/br/com/wbotelhos/starting/util/Image.java
+++ src/br/com/wbotelhos/starting/util/Image.java
@@ -2,23 +2,20 @@ package br.com.wbotelhos.starting.util;
 
 public class Image {
 
-	private static final String[] ARQUIVOS_PERMITIDOS = { ".jpg", ".jpeg", ".gif", ".bmp", ".png" };
+	private static final String[] ALLOWED_FILE = { ".jpg", ".jpeg", ".gif", ".bmp", ".png" };
 
 	public static String getExtension(String fileName) {
-		String extensao = (fileName.lastIndexOf(".") > 0) ? fileName.substring(fileName.lastIndexOf(".")) : "";
+		int last = fileName.lastIndexOf(".");
+		String extensao = (last > 0) ? fileName.substring(last) : "";
 		return (extensao.equalsIgnoreCase(".jpeg") ? ".jpg" : extensao).toLowerCase();
 	}
 
-	public static boolean isValidExtension(String extension) {
-		int tam = extension.length();
+	public static boolean isValidFile(String fileName) {
+		String extension = getExtension(fileName);
 
-		if (tam == 4 || (tam == 5 && extension.equalsIgnoreCase(".jpeg"))) {
-			extension = extension.toLowerCase();
-
-			for (int i = 0; i < ARQUIVOS_PERMITIDOS.length; i++) {
-				if (extension.endsWith(ARQUIVOS_PERMITIDOS[i])) {
-					return true;
-				}
+		for (String item : ALLOWED_FILE) {
+			if (extension.endsWith(item)) {
+				return true;
 			}
 		}
 
