b7012ad46812291c58ff84e22e7c9d58800da4e5
Washington Botelho <wbotelhos@gmail.com>
2011-04-06 18:01:00 -0300

    Método de upload e download de thumb e imagem.

--- src/br/com/wbotelhos/starting/controller/UsuarioController.java
+++ src/br/com/wbotelhos/starting/controller/UsuarioController.java
@@ -5,19 +5,30 @@ import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.Validator;
+import br.com.caelum.vraptor.core.Localization;
+import br.com.caelum.vraptor.interceptor.download.InputStreamDownload;
+import br.com.caelum.vraptor.interceptor.multipart.UploadedFile;
+import br.com.caelum.vraptor.validator.Validations;
+import br.com.wbotelhos.starting.exception.UploadException;
 import br.com.wbotelhos.starting.model.Usuario;
 import br.com.wbotelhos.starting.model.common.TipoPerfil;
 import br.com.wbotelhos.starting.repository.UsuarioRepository;
+import br.com.wbotelhos.starting.util.Image;
 
 @Resource
 public class UsuarioController {
 
 	private final Result result;
 	private final UsuarioRepository repository;
+	private final Validator validator;
+	private final Localization localization;
 
-	public UsuarioController(Result result, UsuarioRepository repository) {
+	public UsuarioController(Result result, UsuarioRepository repository, Validator validator, Localization localization) {
 		this.result = result;
 		this.repository = repository;
+		this.validator = validator;
+		this.localization = localization;
 	}
 
 	@Get("/usuario/novo")
@@ -60,6 +71,36 @@ public class UsuarioController {
 		result
 		.include("message", "Usu‡rio salvo com sucesso!")
 		.redirectTo(this).exibir(entity);
+	}
+
+	@Post("/usuario/{entity.id}/image")
+	public void uploadImage(Usuario entity, final UploadedFile file) {
+		validator.checking(new Validations(localization.getBundle()) {{
+			that(Image.isValidFile(file.getFileName()), i18n("imagem"), "imagem.invalida");
+	    }});
+
+		validator.onErrorUsePageOf(this).exibir(entity);
+
+		try {
+			repository.uploadImage(entity, file);
+		} catch (UploadException e) {
+			result.include("error", e.getMessage());
+			e.printStackTrace();
+		}
+
+		result.redirectTo(this).exibir(entity);
+	}
+
+	@Get("/usuario/{entity.id}/image/{width}/{height}")
+	public InputStreamDownload viewThumb(Usuario entity, int width, int height) {
+		entity = repository.loadById(entity.getId());
+		return entity.getThumb(entity.getImagePath(), width, height);
+	}
+
+	@Get("/usuario/{entity.id}/image")
+	public InputStreamDownload viewImage(Usuario entity) {
+		entity = repository.loadById(entity.getId());
+		return entity.getImage(entity.getImagePath());
 	}
 
 }
