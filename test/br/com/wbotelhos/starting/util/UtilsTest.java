5b43d0ca4e9dee3468c2bed0152e5c928d1db327
Washington Botelho <wbotelhos@gmail.com>
2011-04-08 19:25:26 -0300

    Teste para keys não encontradas no properties.

--- test/br/com/wbotelhos/starting/util/UtilsTest.java
+++ test/br/com/wbotelhos/starting/util/UtilsTest.java
@@ -1,5 +1,27 @@
 package br.com.wbotelhos.starting.util;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class UtilsTest {
+
+	private String key;
+
+	@Test
+	public void deveriaNaoAcharI18N() {
+		// given
+		dadoQueTenhoAKey("key.inexistente");
+
+		// when
+		String value = Utils.i18n(key);
+
+		// then
+		assertEquals("???key.inexistente???", value);
+	}
+
+	private void dadoQueTenhoAKey(String key) {
+		this.key = key;
+	}
 
 }
