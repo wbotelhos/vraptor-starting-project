dd5113a6230d9238237ffb351087c3c20c6aa831
Washington Botelho <wbotelhos@gmail.com>
2011-04-07 23:19:45 -0300

    Método para consultar um usuário a partir do e-mail e sobrescrito método do usuário para validar se o e-mail já existe no banco.

--- src/br/com/wbotelhos/starting/business/UsuarioBusiness.java
+++ src/br/com/wbotelhos/starting/business/UsuarioBusiness.java
@@ -1,9 +1,12 @@
 package br.com.wbotelhos.starting.business;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
 
 import br.com.caelum.vraptor.ioc.Component;
 import br.com.wbotelhos.starting.business.common.GenericImageBusiness;
+import br.com.wbotelhos.starting.exception.CommonException;
 import br.com.wbotelhos.starting.model.Usuario;
 import br.com.wbotelhos.starting.repository.UsuarioRepository;
 
@@ -14,4 +17,23 @@ public class UsuarioBusiness extends GenericImageBusiness<Usuario> implements Us
 		super(manager);
 	}
 
+	public Usuario getUsuarioByEmail(String email) {
+		try {
+			Query query = manager.createQuery("from Usuario where email = :email");
+			query.setParameter("email", email);
+			return (Usuario) query.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
+
+	@Override
+	public Usuario save(Usuario entity) throws CommonException {
+		if (this.getUsuarioByEmail(entity.getEmail()) != null) {
+			throw new CommonException("email.ja.cadastrado");
+		}
+
+		return super.save(entity);
+	}
+	
 }
