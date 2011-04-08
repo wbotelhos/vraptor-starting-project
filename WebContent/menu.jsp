e67780cc4b63637f0a63a905568e010064820215
Washington Botelho <wbotelhos@gmail.com>
2011-04-08 14:10:54 -0300

    Novo método de I18N com alteração client e server side. :D

--- WebContent/menu.jsp
+++ WebContent/menu.jsp
@@ -4,19 +4,15 @@
 	<a href="javascript:void(0);" onclick="confirmar('title', 'yes', 'no', 'confirm?', dummy);">Confirmar</a>
 
 	<c:if test="${userSession.user != null}">
-		| <a href="<c:url value='/usuario' />">Usu&aacute;rios</a> | <a href="<c:url value='/usuario/novo' />">Novo Usu&aacute;rio</a>
+		| <a href="<c:url value='/usuario' />">Usu&aacute;rios</a> | <a href="<c:url value='/usuario/criar' />">Novo Usu&aacute;rio</a>
 	</c:if>
 </div>
 
-<c:set var="uri" value="${fn:replace(pageContext.request.requestURI, pageContext.request.contextPath, '')}"/>
-
 <div id="identification">
-    [ <a href="<c:url value='${uri}?br'/>">BR</a>&nbsp; | <a href="<c:url value='${uri}?us'/>">US</a> ] <fmt:message key="bem.vindo" />:
+    [ <a href="<c:url value='/translate/pt/br'/>">BR</a>&nbsp; | <a href="<c:url value='/translate/en/us'/>">US</a> ] <fmt:message key="bem.vindo" />:
 
 	<c:choose>
-		<c:when test="${userSession.user == null}">
-			Visitante
-		</c:when>
+		<c:when test="${userSession.user == null}">Visitante</c:when>
 		<c:otherwise>
 			${userSession.user.nome} (<a href="<c:url value='/logout'/>">sair</a>)
 		</c:otherwise>
--- src/br/com/wbotelhos/starting/controller/IndexController.java
+++ src/br/com/wbotelhos/starting/controller/IndexController.java
@@ -1,17 +1,24 @@
 package br.com.wbotelhos.starting.controller;
 
+import static br.com.caelum.vraptor.view.Results.referer;
+
+import java.util.Locale;
+
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.com.wbotelhos.starting.component.UserSession;
 
 @Resource
 public class IndexController {
 
 	private final Result result;
+	private final UserSession userSession;
 
-	public IndexController(Result result) {
+	public IndexController(Result result, UserSession userSession) {
 		this.result = result;
+		this.userSession = userSession;
 	}
 
 	@Path("/")
@@ -27,6 +34,19 @@ public class IndexController {
 	@Get("/500")
 	public void erro500() {
 		result.forwardTo("/500.jsp");
+	}
+	
+	@Get("/translate/{language}/{country}")
+	public void translateTo(String language, String country) {
+		try {
+			Locale.setDefault(new Locale(language, country));
+
+			userSession.setLanguage(language + "_" + country);
+
+		    result.use(referer()).redirect();
+		} catch (IllegalStateException e) {
+		    result.redirectTo(this).index();
+		}
 	}
 
 }
