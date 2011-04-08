e2b52a9e76a803fffcff6a6e79844bc277d6f8ce
Washington Botelho <wbotelhos@gmail.com>
2011-04-08 19:03:53 -0300

    Injetado usuário.

--- test/br/com/wbotelhos/starting/controller/IndexControllerTest.java
+++ test/br/com/wbotelhos/starting/controller/IndexControllerTest.java
@@ -2,24 +2,26 @@ package br.com.wbotelhos.starting.controller;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.util.test.MockResult;
-import br.com.wbotelhos.starting.controller.IndexController;
+import br.com.wbotelhos.starting.component.UserSession;
 
 public class IndexControllerTest {
 
 	private IndexController controller;
 
-	@Spy
-	private Result result = new MockResult();
+	@Mock private UserSession userSession;
+
+	@Spy private Result result = new MockResult();
 
 	@Before
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
-		controller = new IndexController(result);
+		controller = new IndexController(result, userSession);
 	}
 
 	@Test
