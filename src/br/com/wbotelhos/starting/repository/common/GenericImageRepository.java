572ae167d9881193658458a21703c206ab8847b0
Washington Botelho <wbotelhos@gmail.com>
2011-04-09 12:37:52 -0300

    Assinatura do método que envia uma imagem para a galeria e o que salva a galeria.

--- src/br/com/wbotelhos/starting/repository/common/GenericImageRepository.java
+++ src/br/com/wbotelhos/starting/repository/common/GenericImageRepository.java
@@ -5,13 +5,18 @@ import java.io.FileNotFoundException;
 import br.com.caelum.vraptor.interceptor.multipart.UploadedFile;
 import br.com.wbotelhos.starting.exception.UploadException;
 import br.com.wbotelhos.starting.model.common.AbstractImage;
+import br.com.wbotelhos.starting.model.common.AbstractImageGallery;
 
-public interface GenericImageRepository<T extends AbstractImage> {
+public interface GenericImageRepository<T extends AbstractImage, I extends AbstractImageGallery> {
 
 	void removeImage(T entity) throws FileNotFoundException;
 
+	void save(I entityImage);
+
 	void updateImage(T entity);
 
-	void uploadImage(T entity, UploadedFile file) throws UploadException;
+	void uploadImage(T entity, UploadedFile uploadedFile) throws UploadException;
+
+	void uploadGallery(T entity, I entityImage, UploadedFile uploadedFile) throws UploadException;
 
 }
