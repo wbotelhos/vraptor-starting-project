a52c0ac63ec61c164f7ce6b993a247c6be48e1ec
Washington Botelho <wbotelhos@gmail.com>
2011-04-10 17:49:11 -0300

    Testes refatorados. deveriaSave com NullPointer from hell ainda.

--- test/br/com/wbotelhos/starting/business/UsuarioBusinessTest.java
+++ test/br/com/wbotelhos/starting/business/UsuarioBusinessTest.java
@@ -15,20 +15,16 @@ import org.mockito.MockitoAnnotations;
 
 import br.com.wbotelhos.starting.business.UsuarioBusiness;
 import br.com.wbotelhos.starting.model.Usuario;
+import br.com.wbotelhos.starting.model.common.TipoPerfil;
 import br.com.wbotelhos.starting.repository.UsuarioRepository;
 
 public class UsuarioBusinessTest {
 
 	private Usuario entity;
 
-	@Mock
-	private EntityManager manager;
-
-	@Mock
-	private Query query;
-
-	@Mock
-	private UsuarioRepository repository;
+	@Mock private Query query;
+	@Mock private EntityManager manager;
+	@Mock private UsuarioRepository repository;
 
 	@Before
 	public void setUp() {
@@ -42,7 +38,7 @@ public class UsuarioBusinessTest {
 		dadoQueTenhoUmUsuario();
 
 		// when // then
-		when(manager.createQuery("from Usuario")).thenReturn(query);
+		when(manager.createQuery("from " + Usuario.class.getName())).thenReturn(query);
 		when(query.getResultList()).thenReturn(Collections.singletonList(entity));
 	}
 
@@ -78,17 +74,21 @@ public class UsuarioBusinessTest {
 
 		// when
 		repository.save(entity);
+		when(manager.merge(entity)).thenReturn(entity);
 
 		// then
-		when(manager.merge(entity)).thenReturn(entity);
+		verify(manager).createQuery("select id from Usuario where email = :email and (:id is null or id != :id)");
+		verify(query).setParameter("email", entity.getEmail());
+		verify(query).setParameter("id", entity.getId());
 	}
 
 	private void dadoQueTenhoUmUsuario() {
 		entity = new Usuario();
-		entity.setId(System.currentTimeMillis());
+		entity.setId(42l);
 		entity.setNome("Washington Botelho");
 		entity.setEmail("mail@gmail.com");
 		entity.setSenha("password");
+		entity.setPerfil(TipoPerfil.ADMINISTRADOR);
 	}
 
 }
--- test/br/com/wbotelhos/starting/controller/UsuarioControllerTest.java
+++ test/br/com/wbotelhos/starting/controller/UsuarioControllerTest.java
@@ -1,6 +1,11 @@
 package br.com.wbotelhos.starting.controller;
 
+import static br.com.wbotelhos.starting.util.Utils.i18n;
 import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
+
+import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +16,7 @@ import org.mockito.Spy;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.Validator;
 import br.com.caelum.vraptor.core.Localization;
+import br.com.caelum.vraptor.util.test.MockLocalization;
 import br.com.caelum.vraptor.util.test.MockResult;
 import br.com.caelum.vraptor.util.test.MockValidator;
 import br.com.wbotelhos.starting.model.Usuario;
@@ -23,10 +29,10 @@ public class UsuarioControllerTest {
 	private UsuarioController controller;
 
 	@Spy private Result result = new MockResult();
-	@Spy Validator validator = new MockValidator();
+	@Spy private Validator validator = new MockValidator();
+	@Spy private Localization localization = new MockLocalization();
 
 	@Mock private UsuarioRepository repository;
-	@Mock private Localization localization;
 
 	@Before
 	public void setUp() {
@@ -35,7 +41,33 @@ public class UsuarioControllerTest {
 	}
 
 	@Test
-	public void deveriaEditar() throws Exception {
+	public void deveriaAtualizar() throws Exception {
+		// given
+		dadoQueTenhoUmUsuario();
+
+		// when
+		controller.atualizar(entity);
+
+		// then
+		verify(repository).save(entity);
+		verify(result).include("message", i18n("usuario.atualizado.sucesso"));
+	}
+
+	@Test
+	public void deveriaCriar() {
+		// given
+		dadoQueTenhoUmUsuario();
+
+		// when
+		controller.criar(entity);
+
+		// then
+		verify(result).include("perfilList", TipoPerfil.values());
+		verify(result).include("entity", entity);
+	}
+
+	@Test
+	public void deveriaChamarFormularioDeEdicaoSemBuscarNoBanco() {
 		// given
 		dadoQueTenhoUmUsuario();
 
@@ -43,12 +75,27 @@ public class UsuarioControllerTest {
 		controller.editar(entity);
 
 		// then
-		verify(repository).loadById(entity.getId());
-		
+		verify(result).include("perfilList", TipoPerfil.values());
+		verify(result).include("entity", entity);
 	}
 
 	@Test
-	public void deveriaExibir() throws Exception {
+	public void deveriaChamarFormularioDeEdicaoBuscandoNoaBanco() {
+		// given
+		dadoQueReceboUmUsuarioSomenteComIDParaEditar();
+
+		// when
+		controller.editar(entity);
+
+		// then
+		verify(result).include("perfilList", TipoPerfil.values());
+
+		Usuario resulted = verify(repository).loadById(entity.getId());
+		verify(result).include("entity", resulted);
+	}
+
+	@Test
+	public void deveriaExibir() {
 		// given
 		dadoQueTenhoUmUsuario();
 
@@ -61,7 +108,19 @@ public class UsuarioControllerTest {
 	}
 
 	@Test
-	public void deveriaRemover() throws Exception {
+	public void deveriaListar() {
+		// given
+
+		// when
+		controller.listagem();
+
+		// then
+		verify(repository).loadAll();
+		verify(result).include("entityList", new ArrayList<Usuario>());
+	}
+
+	@Test
+	public void deveriaRemover() {
 		// given
 		dadoQueTenhoUmUsuario();
 
@@ -70,7 +129,7 @@ public class UsuarioControllerTest {
 
 		// then
 		verify(repository).remove(entity);
-		verify(result).include("message", "Usu‡rio removido com sucesso!");
+		verify(result).include("message", i18n("usuario.removido.sucesso"));
 	}
 	
 	@Test
@@ -82,17 +141,24 @@ public class UsuarioControllerTest {
 		controller.salvar(entity);
 
 		// then
+		Assert.assertEquals("default.jpg", entity.getImagem());
+
 		verify(repository).save(entity);
-		verify(result).include("message", "Usu‡rio salvo com sucesso!");
+		verify(result).include("message", i18n("usuario.salvo.sucesso"));
 	}
-	
+
 	private void dadoQueTenhoUmUsuario() {
 		entity = new Usuario();
-		entity.setId(System.currentTimeMillis());
+		entity.setId(42l);
 		entity.setNome("Washington Botelho");
 		entity.setEmail("mail@gmail.com");
 		entity.setSenha("password");
 		entity.setPerfil(TipoPerfil.ADMINISTRADOR);
+	}
+	
+	private void dadoQueReceboUmUsuarioSomenteComIDParaEditar() {
+		entity = new Usuario();
+		entity.setId(42l);
 	}
 
 }
