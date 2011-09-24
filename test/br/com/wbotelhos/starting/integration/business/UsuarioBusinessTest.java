2fc26c189c95ac19302df0ea1b56cb6e183a1cfc
Washington Botelho <wbotelhos@gmail.com>
2011-09-23 23:17:37 -0300

    Refatorado os testes de integração do usuário.

--- test/br/com/wbotelhos/starting/integration/business/UsuarioBusinessTest.java
+++ test/br/com/wbotelhos/starting/integration/business/UsuarioBusinessTest.java
@@ -1,8 +1,11 @@
 package br.com.wbotelhos.starting.integration.business;
 
-import static org.junit.Assert.assertEquals;
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.List;
 
@@ -18,19 +21,16 @@ import org.junit.Test;
 import br.com.wbotelhos.starting.business.UsuarioBusiness;
 import br.com.wbotelhos.starting.exception.CommonException;
 import br.com.wbotelhos.starting.helper.Given;
+import br.com.wbotelhos.starting.helper.Helper;
 import br.com.wbotelhos.starting.model.Usuario;
-import br.com.wbotelhos.starting.model.UsuarioImage;
-import br.com.wbotelhos.starting.model.common.Perfil;
 import br.com.wbotelhos.starting.repository.UsuarioRepository;
 
 public class UsuarioBusinessTest {
 
-	private static final String DATASET_USUARIO = "/br/com/wbotelhos/starting/integration/xml/Usuario.xml";
-	private static final String DATASET_USUARIO_IMAGE = "/br/com/wbotelhos/starting/integration/xml/UsuarioImage.xml";
-	private static final Long ID_VALIDO = 1L;
+	private DBUnitHelper dbUnitHelper = new DBUnitHelper();
+	private Helper helper = new Helper(dbUnitHelper);
 
 	private UsuarioRepository repository;
-	private DBUnitHelper dbUnitHelper = new DBUnitHelper();
 
 	@BeforeClass
 	public static void beforeClass() {
@@ -39,8 +39,7 @@ public class UsuarioBusinessTest {
 
 	@Before
 	public void setup() {
-		dbUnitHelper.cleanInsert(DATASET_USUARIO);
-		dbUnitHelper.cleanInsert(DATASET_USUARIO_IMAGE);
+		helper.clean().insert();
 
 		EntityManager manager = JPAHelper.currentEntityManager();
 
@@ -50,8 +49,7 @@ public class UsuarioBusinessTest {
 	@After
 	public void tearDown() {
 		JPAHelper.close();
-		dbUnitHelper.deleteAll(DATASET_USUARIO_IMAGE);
-		dbUnitHelper.deleteAll(DATASET_USUARIO);
+		//helper.clean();
 	}
 
 	@Test
@@ -62,121 +60,136 @@ public class UsuarioBusinessTest {
 		List<Usuario> entityList = (List<Usuario>) repository.loadAll();
 
 		// then
-		assertEquals("numero de registros encontrados", 3, entityList.size());
+		assertEquals("deve retornar a quantidade correta de registros", 3, entityList.size());
 
-		verificarEntity(1l, "email_1@gmail.com", "imagem-1.jpg", "nome-1", Perfil.MEMBRO, "senha-1", entityList.get(0));
-		verificarEntity(2l, "email_2@gmail.com", "imagem-2.jpg", "nome-2", Perfil.MODERADOR, "senha-2", entityList.get(1));
-		verificarEntity(3l, "email_3@gmail.com", "imagem-3.jpg", "nome-3", Perfil.ADMINISTRADOR, "senha-3", entityList.get(2));
+		verifyEntities(Given.usuario(1l), entityList.get(0));
+		verifyEntities(Given.usuario(2l), entityList.get(1));
+		verifyEntities(Given.usuario(3l), entityList.get(2));
 	}
 
 	@Test
 	public void deveriaBuscarPorID() {
 		// given
-		Usuario found = repository.loadById(ID_VALIDO);
+		Long expected = 1l;
+
+		Usuario actual = repository.loadById(expected);
 
 		// when
-		assertNotNull("usuario encontrado", found);
+		assertNotNull("usuario encontrado", actual);
 
 		// then
-		verificarEntity(1l, "email_1@gmail.com", "imagem-1.jpg", "nome-1", Perfil.MEMBRO, "senha-1", found);
+		verifyEntities(Given.usuario(expected), actual);
 	}
 
 	@Test
 	public void deveriaSalvar() throws CommonException {
 		// given
-		Usuario expected = Given.usuario(ID_VALIDO, "email_1@email.com", "imagem-1.jpg", "nome-1", Perfil.ADMINISTRADOR, "senha-1");
+		Usuario entity = Given.usuario(null);
+		entity.setImageList(Given.usuarioImageList(1l, 1l, 2l, 3l));
+
+		Usuario expected = Given.usuario(4l);
 
 		// when
-		repository.save(expected);
+		Usuario actual = repository.save(entity);
+
+		List<Usuario> actualList = (List<Usuario>) repository.loadAll();
 
 		// then
-		Usuario found = repository.loadById(expected.getId());
+		assertEquals("deve ter um registro a mais", 4, actualList.size());
+		assertEquals("deve salvar as imagens", 3, entity.getImageList().size());
 
-		assertNotNull("usuario encontrado", found);
+		verifyEntities(expected, actual);
+	}
 
-		verificarEntities(expected, found);
+	@Test
+	public void deveriaLancarExceptionCustomizadaAoSalvarComEmailJaCadastrado() {
+		// given
+		Usuario entity = Given.usuario(null);
+		entity.setEmail("email-1@mail.com");
+
+		try {
+			// when
+			repository.save(entity);
+			fail("exception esperada, mas nao lancada");
+		} catch (CommonException e) {
+			// then
+			assertEquals("email.ja.cadastrado", e.getMessage());
+		}
 	}
 
 	@Test
 	public void deveriaAtualizar() throws CommonException {
 		// given
-		Usuario altered = repository.loadById(ID_VALIDO);
+		Usuario expected = Given.usuario(4l); // novos dados
+		expected.setId(1l);
 
 		// when
-		altered.setEmail("novo_email@mail.com");
-		altered.setImagem("nova-imagem.jpg");
-		altered.setNome("novo-nome");
-		altered.setPerfil(Perfil.MEMBRO);
-		altered.setSenha("nova-senha");
+		repository.save(expected);
 
-		repository.save(altered);
-
-		Usuario found = repository.loadById(ID_VALIDO);
+		Usuario actual = repository.loadById(expected.getId());
 
 		// then
-		verificarEntities(altered, found);
+		verifyEntities(expected, actual);
 	}
 
 	@Test
 	public void deveriaRemover() throws CommonException {
 		// given
-		Usuario found = repository.loadById(ID_VALIDO);
+		Long id = 1l;
+		Usuario entity = repository.loadById(id);
 
 		// when
-		repository.remove(found);
+		repository.remove(entity);
 
 		// then
-		found = repository.loadById(ID_VALIDO);
+		Usuario actual = repository.loadById(id);
 
-		assertNull("usuario nao encontrado", found);
+		assertNull("o registro nao deve ser encontrado", actual);
 	}
-
 
 	@Test
 	public void deveriaBuscarPorId() {
-		Usuario entity = repository.loadById(ID_VALIDO);
+		// given
+		Long id = 1l;
 
-		assertNotNull("usuario nao encontrado", entity);
+		// when
+		Usuario actual = repository.loadById(id);
+
+		// then
+		assertNotNull("deve encontrar o registro", actual);
 	}
 
-	private void verificarEntity(Long id, String email, String imagem, String nome, Perfil perfil, String senha, Usuario found) {
-		verificarEntity(id, email, imagem, nome, perfil, senha, found.getImageList(), found);		
+	@Test
+	public void deveriaAfirmarExistenciaDeUmEmailJaExistente() {
+		// given
+		Usuario entity = Given.usuario(1l); // email existente
+		entity.setId(null); // novo cadastro
+
+		// when
+		Boolean actual = repository.isMailExist(entity);
+
+		// then
+		assertTrue("deve afirmar a existencia", actual);
 	}
 
-	private void verificarEntity(Long id, String email, String imagem, String nome, Perfil perfil, String senha, List<UsuarioImage> imageList, Usuario found) {
-		assertEquals("usuario.id", id, found.getId());
-		assertEquals("usuario.email", email, found.getEmail());
-		assertEquals("usuario.imagem", imagem, found.getImagem());
-		assertEquals("usuario.nome", nome, found.getNome());
-		assertEquals("usuario.perfil", perfil, found.getPerfil());
-		assertEquals("usuario.senha", senha, found.getSenha());
+	@Test
+	public void deveriaNaoAfirmarExistenciaDeUmEmailJaExistentePoremMeuProprio() {
+		// given
+		Usuario entity = Given.usuario(1l);
 
-		UsuarioImage expectedImage = imageList.get(0);
-		UsuarioImage foundImage = found.getImageList().get(0);
+		// when
+		Boolean actual = repository.isMailExist(entity);
 
-		assertEquals("usuarioImage.id", expectedImage.getId(), foundImage.getId());
-		assertEquals("usuarioImage.descricao", expectedImage.getDescricao(), foundImage.getDescricao());
-		assertEquals("usuarioImage.imagemNome", expectedImage.getImagem(), foundImage.getImagem());
-		assertEquals("usuarioImage.titulo", expectedImage.getTitulo(), foundImage.getTitulo());
-		assertEquals("usuarioImage.usuario", expectedImage.getUsuario(), foundImage.getUsuario());
+		// then
+		assertFalse("nao deve afirmar a existencia", actual);
 	}
 
-	private void verificarEntities(Usuario expected, Usuario found) {
-		assertEquals("usuario.id", expected.getId(), found.getId());
-		assertEquals("usuario.email", expected.getEmail(), found.getEmail());
-		assertEquals("usuario.imagem", expected.getImagem(), found.getImagem());
-		assertEquals("usuario.nome", expected.getNome(), found.getNome());
-		assertEquals("usuario.perfil", expected.getPerfil(), found.getPerfil());
-		assertEquals("usuario.senha", expected.getSenha(), found.getSenha());
-
-		UsuarioImage expectedImage = expected.getImageList().get(0);
-		UsuarioImage foundImage = found.getImageList().get(0);
-
-		assertEquals("usuarioImage.id", expectedImage.getId(), foundImage.getId());
-		assertEquals("usuarioImage.descricao", expectedImage.getDescricao(), foundImage.getDescricao());
-		assertEquals("usuarioImage.imagem", expectedImage.getImagem(), foundImage.getImagem());
-		assertEquals("usuarioImage.titulo", expectedImage.getTitulo(), foundImage.getTitulo());
-		assertEquals("usuarioImage.usuario.id", expectedImage.getUsuario().getId(), foundImage.getUsuario().getId());
+	private void verifyEntities(Usuario expected, Usuario found) {
+		assertEquals("deve ter o mesmo email", expected.getEmail(), found.getEmail());
+		assertEquals("deve ter a mesmo imagem", expected.getImagem(), found.getImagem());
+		assertEquals("deve ter o mesmo nome", expected.getNome(), found.getNome());
+		assertEquals("deve ter o mesmo perfil", expected.getPerfil(), found.getPerfil());
+		assertEquals("deve ter a mesmo senha", expected.getSenha(), found.getSenha());
 	}
 
 }
